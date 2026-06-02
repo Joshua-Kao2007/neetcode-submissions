9cb2e926657c64e20261ea9aadccd60d15f62e24
Joshua-Kao2007 <kaojoshua0@gmail.com>
2026-06-02 17:15:35 -0500

    Add: remove-duplicates-from-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # Given an integer array sorted in non-decreasing order..so increasing with chance of equal signs
+        # Remove duplicates in place such that each unique element can only appear at most twice
+        # nums = [1,1,1,2,2,3] --> nums = [1,1,2,2,3,_] --> this would return 5
+        # One pointer k simply points to the place in memory of the bad index
+        # have index L traversing the list
+        LENGTH = len(nums)
+        counter = {}
+        L = 0
+        for R in range(LENGTH):
+            num = nums[R]
+            if num not in counter:
+                counter[num] = 0
+            counter[num]+= 1
+            if counter[num] > 2:
+                continue
+            nums[L] = num
+            L += 1
+        return L
