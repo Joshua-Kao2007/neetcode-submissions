31a4ccd104a33c385b28fe917f8d6cd469018a61
Joshua-Kao2007 <kaojoshua0@gmail.com>
2026-06-11 18:40:58 -0400

    Add: is-anagram - submission-29

---
+++ Data Structures & Algorithms/is-anagram/submission-29.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+        # return Counter(s) == Counter(t)
+        # s_chr = [0]*26
+        # t_chr = [0]*26
+        # for char in s:
+        #     s_chr[ord(char)-ord('a')] += 1
+        # for char in t:
+        #     t_chr[ord(char)-ord('a')] += 1
+        # return s_chr == t_chr
